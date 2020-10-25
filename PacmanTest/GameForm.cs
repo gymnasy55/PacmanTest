297d9d57e823f56bb99d620bfdcbd0beefba6046
School55 Code Team <school55codeteam@gmail.com>
2020-10-25 12:17:42 +0200

    Initial commit

---
+++ PacmanTest/GameForm.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PacmanTest
+{
+    public partial class GameForm : Form
+    {
+        private readonly Pacman _pacman;
+        private Keys _key;
+        int delta;
+
+        public GameForm()
+        {
+            InitializeComponent();
+            _pacman = new Pacman();
+            this.BackColor = Color.Black;
+            this.ClientSize = new System.Drawing.Size(402, 402);
+            _key = Keys.Right;
+            delta = 2;
+        }
+
+        private void GameForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Right:
+                    _key = Keys.Right;
+                    break;
+                case Keys.Up:
+                    _key = Keys.Up;
+                    break;
+                case Keys.Left:
+                    _key = Keys.Left;
+                    break;
+                case Keys.Down:
+                    _key = Keys.Down;
+                    break;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            switch (_key)
+            {
+                case Keys.Right:
+                    _pacman.X += delta;
+                    break;
+                case Keys.Up:
+                    _pacman.Y -= delta;
+                    break;
+                case Keys.Left:
+                    _pacman.X -= delta;
+                    break;
+                case Keys.Down:
+                    _pacman.Y += delta;
+                    break;
+            }
+            pictureBox1.Invalidate();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawRectangle(Pens.Blue, new Rectangle(1, 1, 400, 400));
+            e.Graphics.FillEllipse(Brushes.Yellow, _pacman.X, _pacman.Y, _pacman.Diameter, _pacman.Diameter);
+        }
+    }
+}
---
+++ PacmanTest/Pacman.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace PacmanTest
+{
+    internal class Pacman
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Diameter { get; } = 20; 
+
+        public Pacman()
+        {
+            this.X = 0;
+            this.Y = 0;
+        }
+
+        public Pacman(int X, int Y)
+        {
+            this.X = X;
+            this.Y = Y;
+        }
+    }
+}
